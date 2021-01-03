56eaff9da0c21f4bdf5ddec09b577041508c4892
Elizabeth Sotomayor <elizabethsotomayor@Elizabeths-MacBook.local>
2021-01-02 21:26:22 -0600

    first-commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+# Set up player, car_manager, and scoreboard objects
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+# Listen for on-screen events and determine turtle movement
+screen.listen()
+screen.onkeypress(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect a successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
